6a6d6b0f0ad89d53156f1910314c0de16836d93c
AndrewRVerdin <AndrewRVerdin@gmail.com>
2013-12-02 11:52:44 -0700

    removed messages
    rm

--- page.php
+++ page.php
@@ -4,6 +4,28 @@
 	<div id="internal-content-inner wrapper" class="inner-bezell bezelled small-13 columns">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
+		<?php
+
+		function breadcrumbs($separator = ' &raquo; ', $home = 'Home') {
+		    $path = array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+		    $base = ($_SERVER['HTTPS'] ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
+		    $breadcrumbs = Array("<a href=\"$base\">$home</a>");
+		    $last = end(array_keys($path));
+
+		    foreach ($path AS $x => $crumb) {
+		        $title = ucwords(str_replace(Array('.php', '_'), Array('', ' '), $crumb));
+		        if ($x != $last)
+		            $breadcrumbs[] = "<a href=\"$base$crumb\">$title</a>";
+		        else
+		            $breadcrumbs[] = $title;
+		    }
+		    return implode($separator, $breadcrumbs);
+		}
+
+		?>
+
+			<p><?= breadcrumbs() ?></p>
+
 
 		<?php
 		//this php code will get the variable that is the scema.org rich snippet
