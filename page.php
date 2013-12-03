e89ee61cbf8ceaf5b30f6cd6a5c92ef5cef7bc80
AndrewRVerdin <AndrewRVerdin@gmail.com>
2013-12-03 12:58:41 -0700

    bc fix

--- page.php
+++ page.php
@@ -6,40 +6,50 @@
 
 		<?php
 
-		function breadcrumbs($separator = ' &raquo; ', $home = 'Home') {
-		    $path = array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
-		    $base = ($_SERVER['HTTPS'] ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
-		    $breadcrumbs = Array("<a href=\"$base\">$home</a>");
-		    $last = end(array_keys($path));
+			function breadcrumbs($separator = ' &raquo; ', $home = 'Home') {
 
-		    foreach ($path AS $x => $crumb) {
-		        $title = ucwords(str_replace(Array('.php', '_'), Array('', ' '), $crumb));
-		        if ($x != $last)
-		            $breadcrumbs[] = "<a href=\"$base$crumb\">$title</a>";
-		        else
-		            $breadcrumbs[] = $title;
-		    }
-		    return implode($separator, $breadcrumbs);
-		}
+			    $path = array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+
+			    $base = (isset($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
+
+				$breadcrumbs = Array("<a href=\"$base\">$home</a>");
+
+				$last = end(array_keys($path));
+
+			    foreach ($path AS $x => $crumb) {
+			        $title = ucwords(str_replace(Array('.php', '_'), Array('', ' '), $crumb));
+			        if ($x != $last)
+			            $breadcrumbs[] = "<a href=\"$base$crumb\">$title</a>";
+			        else
+			            $breadcrumbs[] = $title;
+			    }
+			    return implode($separator, $breadcrumbs);
+
+			}
 
 		?>
 
-			<p><?= breadcrumbs() ?></p>
+		<p><?= breadcrumbs() ?></p>
+
+
 
 
 		<?php
-		//this php code will get the variable that is the scema.org rich snippet
-		//some edits here, hope this works.//
-		//setup a default value if none is found in the meta data of the post
-		$itemType = '';
 
-		$metaDataValues = get_post_custom_values('richsnippet' , get_the_ID() );
+			//this php code will get the variable that is the scema.org rich snippet
+			//setup a default value if none is found in the meta data of the post
+			$itemType = '';
 
-		//check to make sure that the keyvalue is there
 
-		//set the $itmeType to the value using the key
+			$metaDataValues = get_post_custom_values('richsnippet' , get_the_ID() );
+
+			//check to make sure that the keyvalue is there
+
+			//set the $itmeType to the value using the key
 		
 		?>
+
+		
 		<article>
 			<div class="large-9 columns" id="internal-content" itemscope itemtype="<?php echo $itemType; ?>" >
 					<h1 class="caps"><?php the_title(); ?></h1>
