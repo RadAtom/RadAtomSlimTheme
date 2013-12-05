2b6a10e3055b4dd79abf8c2a140c2fe6b655ffcb
AndrewRVerdin <AndrewRVerdin@gmail.com>
2013-12-05 14:55:30 -0700

    check that meta!
    WHUT?!

--- helpers/pages.php
+++ helpers/pages.php
@@ -3,7 +3,7 @@
 /**
  * Calls the class on the post edit screen.
  */
-function call_someClass() {
+function call_RadAtomWordpressPages() {
     new RadAtomWordpressPages();
 }
 
@@ -13,37 +13,57 @@ if ( is_admin() ) {
 
 class RadAtomWordpressPages {
 	function __construct() {
-    	add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
+    	add_action( 'add_meta_boxes', array( $this, 'add_ra_snippet' ) );
 		add_action( 'save_post', array( $this, 'save' ) );
    }
 
 
-	public function add_meta_box() {
+	public function add_ra_snippet() {
 		add_meta_box(
-			'schema_snippet'
-			,'Schema Rich Snippet'
-			,array( $this, 'render_meta_box_content' )
+			'ra_snippet'
+			,__( 'Rad Atom Snippet Box', 'snippet_textdomain')
+			,array( $this, 'render_ra_snippet_box' )
 			,'page'
 			,'advanced'
 			,'high'
 		);
 	}
 
-	public function save() {
 
+	public function save( $post_id ) {
+
+		$nonce = $_POST['ra_snippet_box_nonce'];
+
+
+		//if there is autosave
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+			return $post_id;
+		//check permissions
+		if ( 'page' == $_POST['post_type'] ) {
+			if ( ! current_user_can( 'edit_page', $post_id) )
+				return $post_id;
+		}
+		else {
+			if ( ! current_user_can( 'edit_post', $post_id) )
+				return $post_id;
+		}
+
+	$snippet = sanitize_text_field( $_POST['ra_snippet_field'] );
+
+	update_post_meta( $post_id, 'ra_snippet_field', $snippet );
+	
 	}
 
-	public function render_meta_box_content( $post ) {
-		wp_nonce_field( 'rad_atom_snippet_box', 'rad_atom_snippet_box_nonce');
+	
+	public function render_ra_snippet_box( $page ) {
+		wp_nonce_field( 'ra_snippet_box', 'ra_snippet_box_nonce');
 
-		$value = get_post_meta( $post->ID, 'schema_key', true);
-		//echo var_dump($value);	
-
-		echo '<label for="snippet_field">';
-		_e('Description', 'snippit_textdomain');
+		echo '<label for="ra_snippet_field">';
+		_e('Description', 'snippet_textdomain');
 		echo '</label>';
-		echo '<input type="text" id="snippet_field" name="snippet_field" value="'. $value.'" size="25"/>';
+		echo '<input type="text" id="ra_snippet_field" name="ra_snippet_field" value="'. get_post_meta( $page->ID, 'ra_snippet_field', true).'" size="25"/>';
 	}
+
 
 }
 
