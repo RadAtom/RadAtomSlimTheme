b5579ad8840e45a3ae19d3151903d5ace9697000
AndrewRVerdin <AndrewRVerdin@gmail.com>
2013-12-04 17:12:33 -0700

    meta box update
    no “array”

--- helpers/pages.php
+++ helpers/pages.php
@@ -36,13 +36,13 @@ class RadAtomWordpressPages {
 	public function render_meta_box_content( $post ) {
 		wp_nonce_field( 'rad_atom_snippet_box', 'rad_atom_snippet_box_nonce');
 
-		$value = get_post_meta( $post->ID, 'schema_key');
-		echo var_dump($value);
+		$value = get_post_meta( $post->ID, 'schema_key', true);
+		//echo var_dump($value);	
 
 		echo '<label for="snippet_field">';
 		_e('Description', 'snippit_textdomain');
 		echo '</label>';
-		echo '<input type="text" id="snippet_field" name="snippet_field" value="' . esc_attr( $value ) .'" size="25"/>';
+		echo '<input type="text" id="snippet_field" name="snippet_field" value="'. $value.'" size="25"/>';
 	}
 
 }
