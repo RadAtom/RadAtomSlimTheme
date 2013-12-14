cd033d322311cd3b4686d9b519bf077c8bbef262
AndrewRVerdin <AndrewRVerdin@gmail.com>
2013-12-13 22:47:42 -0700

    JQuery/CSS R4
    Round 1 header is just about finished, just need to get that toggleable
    feature to happen

--- js/radatom.js
+++ js/radatom.js
@@ -1,15 +1,23 @@
 function show_nav_menu(){
-  $('#social-menu').hide();
   $('#nav-menu').toggle();
+  $('#social-menu').hide();
   $('#navbuttonbox').toggleClass('navbuttonbox');
+  //the following code is just a fragment. meaning that the following code does not actually belong. you see i am trying to make the animations be toggleable on click for the navbuttonbox and nav-menu
+  $('#navbuttonbox').animate({marginLeft: "-1em"}, 400);
+  $('#nav-menu').animate({width: "+19.9em"}, 400);
   $('#socialbuttonbox').removeClass('socialbuttonbox');
 }
 
 function show_social_menu(){
-  $('#nav-menu').hide();
   $('#social-menu').toggle();
+  $('#nav-menu').hide();
   $('#socialbuttonbox').toggleClass('socialbuttonbox');
+  //same goes for this code
+  $('#socialbuttonbox' ).animate({marginRight: "4em"}, 400);
+  $('#social-menu').animate({width: "+19.9em"}, 400);
   $('#navbuttonbox').removeClass('navbuttonbox');
+  
+  
 }
 
 $( document ).ready(function() {
