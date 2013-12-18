f01a4ce78879c01b1fa6c0b7c1d6b4677ac84fb2
AndrewRVerdin <AndrewRVerdin@gmail.com>
2013-12-18 14:53:52 -0700

    fixed it
    those buttons

--- header.php
+++ header.php
@@ -69,31 +69,28 @@ require_once('radatomautoload.php');
             <h2 class="subheading-text"><?php echo get_bloginfo('description'); ?></h2>
           </a>
         </div>
-
       </div>
-
       <div id="header-stuff" class="large-13 columns">
-
-        <div id="navbuttonbox" class="large-2 columns">
-          <a href="#" id="navbutton" class="round button"><?php echo get_nav_menu_text(); ?></a>
+        <div id="nav-button" class="large-6 columns">
+          <div id="navbuttonbox" class="large-2 columns">
+            <a href="#" id="navbutton" class="round button"><?php echo get_nav_menu_text(); ?></a>
+          </div>
+          <div id="nav-menu" class="large-8 columns">
+            <?php wp_nav_menu( array( 'theme_location' => 'menu-header', 'container'       => 'nav','items_wrap'      => '<ul id="%1$s" class="%2$s inline-list">%3$s</ul>', ) ); ?>
+          </div>
         </div>
-
-        <div id="nav-menu" class="large-8 columns">
-          <?php wp_nav_menu( array( 'theme_location' => 'menu-header', 'container'       => 'nav','items_wrap'      => '<ul id="%1$s" class="%2$s inline-list">%3$s</ul>', ) ); ?>
-        </div>        
-
-        <div id="social-menu"  class="large-8 columns">
-          <ul class="inline-list">
-            <li><a href=""><img src="<?php bloginfo('template_url'); ?>/img/facebook-icon.png" alt="Follow Us On Facebook!"></a></li>
-            <li><a href=""><img src="<?php bloginfo('template_url'); ?>/img/twitter-icon.png" alt="Follow Us On Twitter!"></a></li>
-            <li><a href=""><img src="<?php bloginfo('template_url'); ?>/img/linkedin-icon.png" alt="Follow Us On LinkedIn!"></a></li>
-          </ul>
+        <div id="social-button" class="large-7 columns">
+          <div id="social-menu"  class="large-8 columns">
+            <ul class="inline-list">
+              <li><a href=""><img src="<?php bloginfo('template_url'); ?>/img/facebook-icon.png" alt="Follow Us On Facebook!"></a></li>
+              <li><a href=""><img src="<?php bloginfo('template_url'); ?>/img/twitter-icon.png" alt="Follow Us On Twitter!"></a></li>
+              <li><a href=""><img src="<?php bloginfo('template_url'); ?>/img/linkedin-icon.png" alt="Follow Us On LinkedIn!"></a></li>
+            </ul>
+          </div>
+          <div id="socialbuttonbox" class="large-2 columns">
+            <a href="#" id="socialbutton" class="round button"><?php echo get_social_menu_text(); ?></a>
+          </div>
         </div>
-
-        <div id="socialbuttonbox" class="large-2 columns">
-          <a href="#" id="socialbutton" class="round button"><?php echo get_social_menu_text(); ?></a>
-        </div>
-
       </div>
     </div>
   </div>
